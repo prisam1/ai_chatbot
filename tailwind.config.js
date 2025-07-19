96d73aca4a2f71a558f27c5a54693db99a06afcc
Pritam <pritamsamaddar840@gmail.com>
2025-07-19 11:50:10 +0530

    FIX : Fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./app/**/*.{js,ts,jsx,tsx,mdx}"],
   theme: {
     extend: {
       colors: {
