cba854e840b8fb431fd836bb469ef477a0209584
Pritam <pritamsamaddar840@gmail.com>
2025-07-19 11:32:51 +0530

    FIX : Fix tailwind issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      keyframes: {
-        blink: {
-          "0%, 100%": { opacity: 1 },
-          "50%": { opacity: 0 },
-        },
-        fadeIn: {
-          "0%": { opacity: 0 },
-          "100%": { opacity: 1 },
-        },
+      colors: {
+        background: "#ffffff",
+        foreground: "#000000",
+        border: "hsl(240, 5%, 84%)",
+        ring: "hsl(215, 20%, 65%)",
       },
     },
   },
-  plugins: [require("tailwindcss"), require("autoprefixer")],
+  plugins: [],
 };
