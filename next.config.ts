ec0eedb099b98d03bbd614253d6af67d167b1b54
Pritam <pritamsamaddar840@gmail.com>
2025-07-19 12:09:05 +0530

    FIX : Fix tailwind issue

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,6 @@
-const nextConfig = {
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
   /* config options here */
   env: {
     GOOGLE_API_KEY: process.env.GOOGLE_API_KEY,
